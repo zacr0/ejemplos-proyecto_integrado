8b8667165b1150b320d888911dd475e8cf468a14
Rubenrod18 <rubenrod.018@gmail.com>
2014-03-22 16:43:52 +0100

    Añadiendo HelloWorld

---
+++ Ejemplos_NodeJS/1-HelloWorld/app.js
@@ -0,0 +1,20 @@
+/*
+ * Primero necesitamos crear nuestro
+ * servidor que será un servidor http
+ */
+var http = require('http');
+
+/*
+ * Creamos el servidor y un callback
+ * que contendrá una respuesta(res) y una
+ * solicitud(req)
+ */
+http.createServer(function (req, res) {
+  // Indicamos el tipo de contenido
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World\n'); // Escribimos la respuesta
+  res.end(); // El método end se DEBE de poner
+}).listen(3000); // Escuchamos el servidor por el puerto 3000
+
+// Mensaje de log que aparecerá en la consola
+console.log('Server running at http://localhost:3000/');
